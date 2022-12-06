7a33baef3920efb6b745cacfe7d3be7b9e348124
PedroXA <pedroxad@gmail.com>
2022-12-06 10:00:17 -0300

    UWU

---
+++ Curso Javascript/Section 1 - Javascript Básico/Aula 7 - Constantes/atividade.js
@@ -0,0 +1,12 @@
+const nome = 'Pedro';
+const sobrenome = 'Henrique';
+const idade = 18;
+const peso = 111;
+const alturaEmM = 1.80; // <-- NESTE LOCAL
+
+let IMC = peso / Math.pow(alturaEmM, 2);
+let anoNascimento = 2004;
+
+console.log(IMC)
+console.log(`${nome} ${sobrenome} tem ${idade} anos pesando ${peso} Kg! e tem ${alturaEmM} de altura com seu IMC de ${IMC.toFixed(2)} !`)
+console.log(`${nome} ${sobrenome} nasceu em ${anoNascimento} !`)
---
+++ Curso Javascript/Section 1 - Javascript Básico/Aula 7 - Constantes/index.js
@@ -0,0 +1,35 @@
+/*
+
+1 - São case sensitive
+2 - São imutáveis
+3 - Uso de camelCase
+4 - Sem uso de espaços e traços
+5 - Não pode começar o nome das constantes com número 
+6 - Não podemos criar constantes com palavras reservadas
+
+*/
+
+const nome = 'Jão';
+console.log(nome)
+
+
+// ---------------
+
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+ const conta = primeiroNumero * segundoNumero
+
+console.log(primeiroNumero * segundoNumero);
+// console.log(conta)
+
+
+// ---------------
+
+let resultadoTriplicado = conta * 3;
+console.log(resultadoTriplicado);
+
+// ---------------
+
+
