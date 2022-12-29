74bda3bd3cdb0372b7c2bba1466b88b53ba556a8
Pedro <pedroxad@gmail.com>
2022-12-29 16:18:58 -0300

    29122022

---
+++ Curso Javascript/Section 1 - Javascript Básico/Aula 10 - alert, confirm e prompt (Navegador)/index.js
@@ -0,0 +1,16 @@
+alert('Comando Alerta !')
+// window.alert()
+const confirma = confirm('Deseja seguir com a sua ação ?')
+console.log(confirma)
+// window.confirm()
+const idade = prompt("Digite a sua idade:")
+console.log(idade)
+// window.prompt()
+
+let num1 = prompt("Digite um número: ")
+num1 = parseInt(num1)
+let num2 = prompt("Digite um número: ")
+num2 = parseInt(num2)
+
+let result = num1 + num2
+alert('O resultado foi: ' + result)
