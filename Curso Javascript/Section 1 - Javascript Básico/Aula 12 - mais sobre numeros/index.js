2d59d4891310d5c96c835b2e3b2c942dd4cdeaae
PedroXA <pedroxad@gmail.com>
2023-01-19 20:58:26 -0300

    .

---
+++ Curso Javascript/Section 1 - Javascript Básico/Aula 12 - mais sobre numeros/index.js
@@ -0,0 +1,12 @@
+// IEEE 754-2008 Padrão do javascript para contas
+
+let num1 = 2.5; //  number
+let num2 = 2.5; // number
+
+
+/* console.log(num1 + num2);
+console.log(num1.toString() + num2);
+console.log(typeof num1); */
+
+/* console.log(Number.isInteger(num1)); */
+
---
+++ Curso Javascript/Section 1 - Javascript Básico/Aula 13 - Objeto Math/index.js
@@ -0,0 +1,19 @@
+let num1 = 12.5;
+/* let num2 = Math.floor(num1);  */  /* arredonda pra baixo  */
+/* let num2  = Math.ceil(num1); */  /* arredonda pra cima */
+/* let num2 = Math.round(num1); */ /* arredonda pra cima */
+
+/* console.log(num2) */
+
+console.log(Math.max(10,100,1000,10000));
+console.log(Math.min(10,100,1000,10000));
+
+const random = Math.random() * (10 - 5) + 5; // Vou ter números aleatórios entre 5 e 10
+console.log(random);
+
+console.log(Math.pow(2 , 5))
+console.log(2 ** 5)
+
+// Em javascript pode dividir por 0
+console.log(100 / 0); // Type infinity == True
+
