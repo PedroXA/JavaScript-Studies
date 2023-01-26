9b8bfa146fc0b4c0120b1816e59b53883f818f60
PedroXA <pedroxad@gmail.com>
2023-01-26 16:42:10 -0300

    Aula de objetos

---
+++ Curso Javascript/Section 1 - Javascript Básico/Aula 16 - Objetos/index.js
@@ -0,0 +1,44 @@
+const array = [1,2,3];
+// é possível alterar valores de um array mesmo sendo const mas só alterando o conteúdo dentro || array.push || array.pop || array[0] = 'Alguma coisa' || a regra é só não reatribuir
+
+const pessoa = { // Objeto: {} || array: []
+    /* 
+    nome: [],
+    sobrenome: [],
+    idade: [] 
+    */
+}// este é um objeto literal
+
+
+/* pessoa.nome.push('Pedro','Ricardo','Marcelo', 'Kakw')
+console.log(pessoa) 
+*/
+
+function criarPessoa(nome, sobrenome, idade){
+        return{
+            nome,
+            sobrenome,
+            idade
+           /*  
+            nome: nome,
+            sobrenome: sobrenome,
+            idade: idade 
+            */
+        };
+} // Função factory
+
+const pessoa1 = criarPessoa('Pedro', 'Henrique', 18) // enviando argumentos para o meu parametro da minha função criarPessoa
+/* console.log(pessoa1) */
+
+
+const pessoa2 = { // Objeto: {} || array: []
+    nome: 'Pedro',
+    sobrenome: 'Henrique',
+    idade: 18,
+    
+    fala(){ // É possível criar funções dentro de objetos
+        console.log(`${this.nome} ${this.sobrenome} está falando Oi`)
+    }
+}
+
+/* pessoa2.fala(); */
