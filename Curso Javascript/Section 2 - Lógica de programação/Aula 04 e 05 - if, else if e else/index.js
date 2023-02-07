d6592c29541430f82ef85936ea8dcd67949c1777
PedroXA <pedroxad@gmail.com>
2023-02-07 10:18:22 -0300

    Modelo de formulário

---
+++ Curso Javascript/Section 2 - Lógica de programação/Aula 04 e 05 - if, else if e else/index.js
@@ -0,0 +1,13 @@
+const hora = 17;
+
+/* if (hora >= 0 && hora <= 11) {
+  console.log("Bom dia");
+} 
+else if (hora >= 12 && hora <= 17) {
+  console.log("Boa tarde");
+} 
+else {
+  console.log("Boa noite");
+}
+ */
+
