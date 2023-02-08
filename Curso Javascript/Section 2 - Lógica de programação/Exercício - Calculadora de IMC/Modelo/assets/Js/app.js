125704d5147fe4a3e4522d7099fc41de88f50dfa
PedroXA <pedroxad@gmail.com>
2023-02-08 10:31:27 -0300

    calculadora refatorando

--- Curso Javascript/Section 2 - Lógica de programação/Exercício - Calculadora de IMC/Modelo/assets/Js/app.js
+++ Curso Javascript/Section 2 - Lógica de programação/Exercício - Calculadora de IMC/Modelo/assets/Js/app.js
@@ -1,66 +1,119 @@
-function calculateFunction(){
+// se uma function faz mais de uma coisa, é interessante já fazer outra function a parte (Boas práticas)
 
-    const form = document.querySelector('.form');
-    var person = [];
-    const log = document.querySelector('.log');
-    const stateTypeImc = document.querySelector('.form__stateType');
-
-    function recieveData(event){
-        event.preventDefault();
-        const nome = form.querySelector('.nome').value;
-        let peso = form.querySelector('.peso').value;
-        let altura = form.querySelector('.altura').value;
+const form = document.querySelector(".form");
+const log = document.querySelector(".log"); // Mensagem com os dados do usuário
 
 
-        person.push({
-            nome: nome,
-            peso: peso,
-            altura: altura
-        });
-        console.log(person)
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-        let result = peso / (altura * altura);
+  const inputPeso = e.target.querySelector(".peso");
+  const inputAltura = e.target.querySelector(".altura");
 
-            if(result <= 18.5)
-            {
-                
-                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
-                stateTypeImc.innerHTML = `<h2>Magresa 🦴</h2>`
-            } 
-            else if(result > 18.5 && result <= 24.9)
-            {
-                
-                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
-                stateTypeImc.innerHTML = `<h2>Peso Normal 💪</h2>`
-            } 
-            else if (result >= 25 && result <= 29.9)
-            {
-                
-                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
-                stateTypeImc.innerHTML = `<h2>Sobrepeso 🍖</h2>` 
-            }
-            else if(result >= 30 && result <= 34.9){
-                
-                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
-                stateTypeImc.innerHTML = `<h2>Obesidade Grau 1 💊</h2>`
-                
-            } 
-            else if(result >= 35 && result <= 40){
-                stateTypeImc.innerHTML = ``
-                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
-                stateTypeImc.innerHTML = `<h2>Obesidade Grau 2 💊💊</h2>`
-            }else{
-                stateTypeImc.innerHTML = ``
-                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
-                stateTypeImc.innerHTML = `<h2>Obesidade Grau 3 🌭</h2>`
-            }
-        
-    }
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
 
-   
+  if (!peso) {
+    setResultado("Peso inválido", false);
+    return;
+  }
 
-    form.addEventListener('submit', recieveData);
+  if (!altura) {
+    setResultado("Altura invalida", false);
+    return;
+  }
+
+  const imc = getIMC(peso, altura);
+  const nivelImc = getNivelImc(imc);
+
+    log.innerHTML = `Olá, seu IMC é ${imc.toFixed()}`;
+    state.innerHTML = nivelImc;
+});
+
+function getIMC(peso, altura) {
+  // Function para calcular o IMC
+  const result = peso / (altura * altura);
+  return result.toFixed(2);
 }
-calculateFunction();
+
+function criaP(){
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado(){
+    const state = document.querySelector(".form__stateType");
+    state.innerHTML = '';
+
+    const p = criaP();
+}
+
+function getNivelImc(imc) {
+  // Function para ver qual nível a pessoa está
+  const type = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
+
+  if (imc <= 18.5) {
+    return type[0];
+  } else if (imc > 18.5 && imc <= 24.9) {
+    return type[1];
+  } else if (imc >= 25 && imc <= 29.9) {
+    return type[2];
+  } else if (imc >= 30 && imc <= 34.9) {
+    return type[3];
+  } else if (imc >= 35 && imc <= 40) {
+    return type[4];
+  } else {
+    return type[5];
+  }
+}
 
 
+
+
+
+
+
+
+/* if(result <= 18.5)
+{
+    
+    log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+    stateTypeImc.innerHTML = `<h2>Magresa 🦴</h2>`
+} 
+else if(result > 18.5 && result <= 24.9)
+{
+    
+    log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+    stateTypeImc.innerHTML = `<h2>Peso Normal 💪</h2>`
+} 
+else if (result >= 25 && result <= 29.9)
+{
+    
+    log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+    stateTypeImc.innerHTML = `<h2>Sobrepeso 🍖</h2>` 
+}
+else if(result >= 30 && result <= 34.9){
+    
+    log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+    stateTypeImc.innerHTML = `<h2>Obesidade Grau 1 💊</h2>`
+    
+} 
+else if(result >= 35 && result <= 40){
+    stateTypeImc.innerHTML = ``
+    log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+    stateTypeImc.innerHTML = `<h2>Obesidade Grau 2 💊💊</h2>`
+}else{
+    stateTypeImc.innerHTML = ``
+    log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+    stateTypeImc.innerHTML = `<h2>Obesidade Grau 3 💊💊💊</h2>`
+}
+} */
+/*     calculateFunction(); */
+/* form.addEventListener('submit', recieveData); */
