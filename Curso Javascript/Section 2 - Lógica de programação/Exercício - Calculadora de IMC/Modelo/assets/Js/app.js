1128a6bc5931fea431320b8cb4c15c482f33555c
PedroXA <pedroxad@gmail.com>
2023-02-08 00:23:38 -0300

    calculadora implementação

--- Curso Javascript/Section 2 - Lógica de programação/Exercício - Calculadora de IMC/Modelo/assets/Js/app.js
+++ Curso Javascript/Section 2 - Lógica de programação/Exercício - Calculadora de IMC/Modelo/assets/Js/app.js
@@ -1,14 +1,16 @@
 function calculateFunction(){
 
-    const form = document.querySelector('.form')
+    const form = document.querySelector('.form');
     var person = [];
-    const painel = document.querySelector('.container')
+    const log = document.querySelector('.log');
+    const stateTypeImc = document.querySelector('.form__stateType');
 
     function recieveData(event){
         event.preventDefault();
         const nome = form.querySelector('.nome').value;
         let peso = form.querySelector('.peso').value;
         let altura = form.querySelector('.altura').value;
+
 
         person.push({
             nome: nome,
@@ -19,8 +21,43 @@ function calculateFunction(){
 
         let result = peso / (altura * altura);
 
-        painel.innerHTML += `<p>Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()} </p>`
+            if(result <= 18.5)
+            {
+                
+                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+                stateTypeImc.innerHTML = `<h2>Magresa 🦴</h2>`
+            } 
+            else if(result > 18.5 && result <= 24.9)
+            {
+                
+                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+                stateTypeImc.innerHTML = `<h2>Peso Normal 💪</h2>`
+            } 
+            else if (result >= 25 && result <= 29.9)
+            {
+                
+                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+                stateTypeImc.innerHTML = `<h2>Sobrepeso 🍖</h2>` 
+            }
+            else if(result >= 30 && result <= 34.9){
+                
+                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+                stateTypeImc.innerHTML = `<h2>Obesidade Grau 1 💊</h2>`
+                
+            } 
+            else if(result >= 35 && result <= 40){
+                stateTypeImc.innerHTML = ``
+                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+                stateTypeImc.innerHTML = `<h2>Obesidade Grau 2 💊💊</h2>`
+            }else{
+                stateTypeImc.innerHTML = ``
+                log.innerHTML += `Olá ${nome}, seu peso é de ${peso} kg com a altura de ${altura}, seu IMC é ${result.toFixed()}`
+                stateTypeImc.innerHTML = `<h2>Obesidade Grau 3 🌭</h2>`
+            }
+        
     }
+
+   
 
     form.addEventListener('submit', recieveData);
 }
