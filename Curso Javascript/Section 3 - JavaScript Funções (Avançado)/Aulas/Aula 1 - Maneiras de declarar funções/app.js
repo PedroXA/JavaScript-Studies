29b28b0d3289b8e01bdf027f3c44810c4110525b
PedroXA <pedroxad@gmail.com>
2024-01-23 08:07:44 -0300

    Section 3 - funções

---
+++ Curso Javascript/Section 3 - JavaScript Funções (Avançado)/Aulas/Aula 1 - Maneiras de declarar funções/app.js
@@ -0,0 +1,34 @@
+// Declaração de função (Function hoisting) -> não faz diferença se chamar a função antes do escopo dela
+// falaOi();
+
+function falaOi(){
+    console.log('Oi')
+}
+// falaOi();
+
+
+// First-class (Objetos de primeira classe)
+// Function expression - criar uma variável e atribuir uma função nela
+
+const souUmDado = function(){
+    console.log('Sou um dado.');
+};
+// souUmDado();
+
+function executaFuncao(funcao){
+    funcao();
+}
+executaFuncao(souUmDado);
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function');
+}
+
+// Dentro de um objeto
+const obj = {
+    falar(){
+        console.log('estou falando...');
+    }
+};
+obj.falar();
