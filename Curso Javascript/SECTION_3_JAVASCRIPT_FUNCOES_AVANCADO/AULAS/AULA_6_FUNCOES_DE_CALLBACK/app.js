43bea8ca955c4f9505ae0eb749df8e127e47d82d
Pedro <pedroxad@gmail.com>
2024-02-14 19:21:49 -0300

    Aula funções de callback

---
+++ Curso Javascript/SECTION_3_JAVASCRIPT_FUNCOES_AVANCADO/AULAS/AULA_6_FUNCOES_DE_CALLBACK/app.js
@@ -0,0 +1,26 @@
+// Funções de callback - São funções que 
+
+function rand(min = 1000, max = 5000){
+    const num = Math.random() * (max - min) + min;
+    return Math.floor(num)
+}
+
+
+function f1(callback){
+    setTimeout(function() {
+        console.log('f1')
+        if(callback) callback();
+    }, rand())
+}
+function f2(callback){
+    setTimeout(function() {
+        console.log('f2')
+        if(callback) callback();
+    }, rand())
+}
+function f3(callback){
+    setTimeout(function() {
+        console.log('f3')
+        if(callback) callback();
+    }, rand())
+}
