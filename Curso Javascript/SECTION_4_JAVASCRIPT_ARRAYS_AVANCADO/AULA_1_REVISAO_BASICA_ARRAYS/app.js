43f20c98938f289846e7cfc8c084dde7a9c0332d
Pedro <pedroxad@gmail.com>
2024-03-06 19:30:24 -0300

    aula Revisão básica de array

---
+++ Curso Javascript/SECTION_4_JAVASCRIPT_ARRAYS_AVANCADO/AULA_1_REVISAO_BASICA_ARRAYS/app.js
@@ -0,0 +1,22 @@
+//              0       1       2
+// const nome = ['Pedro', 'Nina', 'Lucas']
+
+/* 
+nome[2] = 'Joao';
+delete nome[1]; 
+*/
+
+// delete -> vai deletar a informação porém não muda o indice dos elementos.
+// console.log(nome)
+
+
+// const animais = new Array('Macaco','Ovelha', 'Tubarão') // forma menos usada de criar um array
+
+// const nome2 = ['Pedro', 'Nina', 'Lucas']
+// const nome3 = [...nome2]; // Copiando dados de um array e colocando em outros por meio do spread
+
+// console.log(nome3)
+
+const nome = 'Pedro Henrique Dos Reis';
+const nomes = nome.split(' '); // transformando minha string em um array separando ela por espaços
+console.log(nomes);
