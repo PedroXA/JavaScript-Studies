664f70845a170bbf1304653dee5ac0385395e4cc
PedroXA <pedroxad@gmail.com>
2024-05-02 18:08:13 -0300

    Aula factory functions

--- Curso Javascript/SECTION_3_JAVASCRIPT_FUNCOES_AVANCADO/AULAS/AULA_8_FACTORY_FUNCTIONS/app.js
+++ Curso Javascript/SECTION_3_JAVASCRIPT_FUNCOES_AVANCADO/AULAS/AULA_8_FACTORY_FUNCTIONS/app.js
@@ -2,35 +2,35 @@
 // Quando uma função está dentro de um objeto ele se chama método
 
 function criaPessoa(nome, sobrenome, altura, peso) {
-  return {
-    nome,
-    sobrenome,
+	return {
+		nome,
+		sobrenome,
 
-    // Getter
-    get nomeCompleto() {
-      return `${this.nome} ${this.sobrenome}`;
-    },
+		// Getter
+		get nomeCompleto() {
+			return `${this.nome} ${this.sobrenome}`;
+		},
 
-    // Setter
-    set nomeCompleto(conteudo) {
-      conteudo = conteudo.split(" "); // vetoriando o valor recebido
-      nome = conteudo.shift(); // tirando o meu
-      sobrenome = conteudo.join(" ");
-    },
+		// Setter
+		set nomeCompleto(conteudo) {
+			conteudo = conteudo.split(" "); // vetoriando o valor recebido
+			nome = conteudo.shift(); // tirando o meu
+			sobrenome = conteudo.join(" ");
+		},
 
-    fala: function () {
-      return `${this.nome} ${sobrenome}`;
-    },
+		fala: function () {
+			return `${this.nome} ${sobrenome}`;
+		},
 
-    altura: altura,
-    peso: peso,
+		altura: altura,
+		peso: peso,
 
-    // Getter
-    get imc() {
-      const indice = this.peso / this.altura ** 2;
-      return indice.toFixed(2);
-    },
-  };
+		// Getter
+		get imc() {
+			const indice = this.peso / this.altura ** 2;
+			return indice.toFixed(2);
+		},
+	};
 }
 // o this sempre vai ser refenciado pelo objeto que está chamando o método
 const p1 = criaPessoa("Pedro", "Henrique", 192, 101);
